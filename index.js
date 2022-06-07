7e46c1799f2e12bea3e26032c1fe250730f89657
Mischka Booley <mischka@gearbox.com.au>
2022-06-07 22:40:43 +1000

    complete filter lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(name =>
+    name.toLowerCase() === string.toLowerCase()
+  )
+}
+
+function fuzzyMatch (drivers, string) {
+  return drivers.filter(name => name.toLowerCase().indexOf(string.toLowerCase()) === 0
+  )
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(name => name.name === string
+    )
+}
